e800e8427fb95d60c87e05b603672878c43c3977
Tobias Mogdans <tobias.mogdans@mayflower.de>
2022-05-09 20:10:21 +0200

    add navitem story

---
+++ frontend/src/stories/NavItem.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {NavItem} from "../Components/Atoms/NavItem";
+
+export default {
+    title: 'Atom/Nav Item',
+    component: NavItem,
+} as ComponentMeta<typeof NavItem>
+
+const Template: ComponentStory<typeof NavItem> = (args) => <NavItem {...args} />
+
+export const Default = Template.bind({});
+Default.args = {
+    item: {
+        name: 'First Item',
+        target: 'main',
+    }
+};
