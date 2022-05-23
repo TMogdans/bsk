0eafecc2f45053b06cd04e1429c71e8ca450560a
Tobias Mogdans <tobias.mogdans@mayflower.de>
2022-05-23 21:57:09 +0200

    add optional className property

--- frontend/src/Components/Atoms/Typography.tsx
+++ frontend/src/Components/Atoms/Typography.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import style from './Typography.module.css';
 import {TypographyStyle} from "../../Types/TypographyType";
 
-type Props = {
+type Properties = {
     type: TypographyStyle;
     text: string;
+    className?: string;
 }
 
-export const Typography = ({type, text}: Props) => {
+export const Typography = ({type, text, className}: Properties) => {
     if (typeof type !== "undefined") {
-        return React.createElement(type.toString(), {className: style[type]}, text);
+        const combinedStyle = [style[type], className].join(" ");
+        return React.createElement(type.toString(), {className: combinedStyle}, text);
     } else return null;
 }
