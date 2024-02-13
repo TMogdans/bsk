0c6e73b3e4a1278887f0272ff5f93c9a4284a566
Tobias Mogdans <tobias.mogdans@mayflower.de>
2024-02-13 21:24:51 +0100

    remove express http api

--- rating-service/api/src/index.ts
+++ rating-service/api/src/index.ts
@@ -1,15 +1,3 @@
-import express, { Request, Response } from "express";
 import { client } from "./pubsub/daprClient";
 
-const app = express();
-const port = process.env.PORT || 3000;
-
 client().then(() => console.log("dapr client started"));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, rating-service! 11515");
-});
-
-app.listen(port, () => {
-  console.log(`Server running at port ${port}`);
-});
