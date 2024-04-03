ef4a0454ae9dfc6fa2f03701a3c3e3404be1df31
Tobias Mogdans <tobias.mogdans@mayflower.de>
2024-04-03 07:28:56 +0200

    log validation errors in base class

--- library-service/api/src/services/baseProcessor.ts
+++ library-service/api/src/services/baseProcessor.ts
@@ -3,7 +3,8 @@ import { AnyObjectSchema } from "yup";
 
 export default abstract class baseProcessor {
   protected async validate(message: BaseMessage, validator: AnyObjectSchema) {
-    await validator.validate(message).catch(() => {
+    await validator.validate(message).catch((e) => {
+      console.log(e);
       throw new Error("Invalid message");
     });
   }
